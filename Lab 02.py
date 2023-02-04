45b7437d2b6bcf303ea4065e2336d55b13577139
kotyara29 <dar.kaliyev@gmail.com>
2023-02-04 16:17:27 +0600

    Lab 01

---
+++ Lab 02.py
@@ -0,0 +1,15 @@
+# Task 1
+
+# Для ошибки SyntaxError:
+try:
+    a == 9
+except NameError:
+    print("You have a name error here")
+
+
+try:
+    9 / 0
+except ZeroDivisionError:
+    print("You have a zero division error here")
+
+
