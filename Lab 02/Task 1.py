6ab5af2ebdb1bef22155680b45de2f40da141c75
kotyara29 <dar.kaliyev@gmail.com>
2023-02-09 18:36:01 +0600

    Lab 01

--- Lab 02/Task 1.py
+++ Lab 02/Task 1.py
@@ -1,15 +1,23 @@
 # Task 1
 
-# Для ошибки SyntaxError:
+# For NameError:
 try:
     a == 9
 except NameError:
     print("You have a name error here")
 
-
+# For ZeroDivisionError
 try:
     9 / 0
 except ZeroDivisionError:
     print("You have a zero division error here")
+
+# For SyntaxError:
+
+try:
+    if 1 == True
+        print('yes')
+except SyntaxError:
+    print('You have syntax error here')
 
 
