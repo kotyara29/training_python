d9720b39e86d5df985fc12e0da464cf3f10e1beb
Darhan Kaliyev <dar.kaliyev@gmail.com>
2023-04-01 14:48:25 +0600

    There's no point of creating abstract class so far. It's not used anywhere. The new_person variable keeps the data entered until captured by the logs.log.

--- Lab_04/Task_1.py
+++ Lab_04/Task_1.py
@@ -1,68 +1,57 @@
-import abc
 import logging
-from abc import ABC, abstractmethod
+from abc import ABC
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG, filename="logs.log", format='%(message)s')
-# logger.setLevel(logging.ERROR)
-
-# logging.debug('This is a debug message')
-# logging.info('This is an info messageblabla')
-# logging.warning('This is a warning message')
-# logging.error('This is an error message')
-# logging.critical('This is a critical message')
 
 
 class AbstractCustoms(ABC):
-    """
-
-    """
-    @abstractmethod
-    def enterdata(self):
-        pass
-
-
-class InlandCustoms(AbstractCustoms):
-    type = "Inland"
+    type = "Undefined"
 
     def __init__(self, name, date):
         self.name = name
         self.date = date
 
-    def enterdata(self):
-        self.name = input("Enter the name: ")
-        self.date = input("Enter the date: ")
-
     def __str__(self):
-        return f"Customs Type: Inland, Name: {self.name}, Date: {self.date}"
+        return f"Customs Type: {self.type}, Name: {self.name}, Date: {self.date}"
+
+
+class InlandCustoms(AbstractCustoms):
+    type = "Inland"
 
 
 class AirCustoms(AbstractCustoms):
     type = "Air"
 
-    def __init__(self):
-        # self.name = name
-        # self.date = date
 
-    def enterdata(self, name, date):
-        self.name = name
-        self.date = date
-        self.name = input("Enter the name: ")
-        self.date = input("Enter the date: ")
-
-    def __str__(self):
-        return f"Customs Type: Air, Name: {self.name}, Date: {self.date}"
+list_overall, list_inland, list_air = [], [], []
 
 
+def show_lists():
+    select_list = input("Type 1 to see the list of all passengers; "
+                        "Type 2 to see the list of aircraft passengers; "
+                        "Type 3 to see the list of inland passengers: ")
+    if select_list == 1:
+        print(list_overall)
+    elif select_list == 2:
+        print(list_air)
+    else:
+        print(list_inland)
 
 
-new_customs = input("Enter 1 for Air or 2 for Inland Customs: ")
-if new_customs == "1":
-    new_person = AirCustoms()
-#     new_person = AirCustoms(new_name, new_date, new_customs)
-else:
-    new_person = InlandCustoms()
-#     new_person = InlandCustoms(new_name, new_date, new_customs)
-# logging.debug(str(new_person).split(', '))
+stop_words = ['STOP', 'END', 'Stop', 'End', 'stop', 'end']
+while input("Enter anything to add data on passenger. Enter STOP or END to finish: ") not in stop_words:
+    if input("Enter 1 for Air or 2 for Inland Customs: ") == "1":
+        new_person = AirCustoms(input("Enter the name: "), input("Enter the date: "))
+        logging.debug(str(new_person).split(', '))
+        list_overall.append(str(new_person).split(', '))
+        list_air.append(str(new_person).split(', '))
+    else:
+        new_person = InlandCustoms(input("Enter the name: "), input("Enter the date: "))
+        logging.debug(str(new_person).split(', '))
+        list_overall.append(str(new_person).split(', '))
+        list_inland.append(str(new_person).split(', '))
+
+show_lists()
 # list_overall.append(new_person)
 # print(list_overall)
